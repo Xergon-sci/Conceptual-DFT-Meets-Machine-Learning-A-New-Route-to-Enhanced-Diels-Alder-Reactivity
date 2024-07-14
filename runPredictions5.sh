8ffc8027913118ac207a0514ee2a64a761b5dbf2
M. Jacobs <jacobs.michiel@icloud.com>
2024-07-14 21:59:04 +0200

    added for calculations

---
+++ runPredictions5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author: M.Jacobs
+
+# Slurm directives
+#SBATCH --job-name=PredictElectrophilicity
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus=1
+
+
+cd $SLURM_SUBMIT_DIR
+
+module purge
+
+module load SciPy-bundle/2023.07-gfbf-2023a
+
+python dirty_calc/predict5.py
